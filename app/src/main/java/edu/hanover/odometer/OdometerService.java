770323eb860fada83d3d6d8245ab0c83465d6bb0
chezema21 <chezema21@hanover.edu>
2018-05-20 19:41:02 -0400

    initial

--- app/src/main/java/edu/hanover/odometer/OdometerService.java
+++ app/src/main/java/edu/hanover/odometer/OdometerService.java
@@ -1,16 +1,87 @@
 package edu.hanover.odometer;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 
 public class OdometerService extends Service {
-    public OdometerService() {
+
+    private final IBinder binder = new OdometerBinder();
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
+    private LocationListener listener;
+    private LocationManager locManager;
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onCreate() {
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String arg0) {
+            }
+
+            @Override
+            public void onProviderDisabled(String arg0) {
+            }
+        };
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listener);
+    }
+
+    @Override
+    public void onDestroy(){
+        if (locManager != null && listener != null) {
+            locManager.removeUpdates(listener);
+            locManager = null;
+            listener = null;
+        }
+    }
+
+    public double getMiles() {
+        return this.distanceInMeters / 1609.344;
     }
 }
